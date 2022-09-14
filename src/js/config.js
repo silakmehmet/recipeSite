aa83b37dd521b8a1462afdebeef58ce6b61e81eb
silakmehmet <82677215+silakmehmet@users.noreply.github.com>
2022-09-14 09:58:03 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e51e8657-d6b0-4519-bcb1-eb7d9020adcc';
+export const MODAL_CLOSE_SEC = 2.5;
